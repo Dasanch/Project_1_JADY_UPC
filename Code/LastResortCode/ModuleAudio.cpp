a316431e01b3bfad318ce7498a52518d6626b4ae
AAurelioGN <monsterlol64@gmail.com>
2018-04-05 01:44:43 +0200

    Updated ModuleAudio

--- Code/LastResortCode/ModuleAudio.cpp
+++ Code/LastResortCode/ModuleAudio.cpp
@@ -6,9 +6,12 @@
 #include "SDL_mixer\include\SDL_mixer.h"
 #pragma comment( lib, "SDL_mixer/libx86/SDL2_mixer.lib" )
 
+
 ModuleAudio::ModuleAudio() : Module()
 {
-	level_music = nullptr;
+	
+	for (uint i = 0; i < MAX_MUSICS; ++i)
+		musics[i] = nullptr;
 	for (uint i = 0; i < MAX_SOUNDEFECTS; ++i)
 		sfx[i] = nullptr;
 }
@@ -28,6 +31,8 @@ bool ModuleAudio::Init()
 		ret = false;
 	}
 
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+
 	return ret;
 }
 
@@ -38,10 +43,16 @@ bool ModuleAudio::CleanUp()
 	LOG("Freeing audios and Mixer library and shutdown mixer");
 
 	for (uint i = 0; i < MAX_SOUNDEFECTS; ++i)
-		if (sfx[i] != nullptr)
+		if (sfx[i] != nullptr) {
 			Mix_FreeChunk(sfx[i]);
+		}
 
-	Mix_FreeMusic(level_music);
+	for (uint i = 0; i < MAX_MUSICS; ++i)
+		if (musics[i] != nullptr) {
+			Mix_FreeMusic(musics[i]);
+		}
+
+	
 
 	Mix_CloseAudio();
 	Mix_Quit();
@@ -49,3 +60,42 @@ bool ModuleAudio::CleanUp()
 	return true;
 }
 
+
+Mix_Chunk* const ModuleAudio::LoadSfx(const char* path) {
+
+	Mix_Chunk *chunk;
+
+	chunk = Mix_LoadWAV(path);
+	if (chunk == NULL)
+	{
+		LOG("Unable to load sfx Mix Error: %s\n", Mix_GetError());
+	}
+	else
+	{
+		sfx[last_chunk++] = chunk;
+	}
+	return chunk;
+}
+
+Mix_Music* const ModuleAudio::LoadMUS(const char* path) {
+
+	Mix_Music *music = NULL;
+
+	music = Mix_LoadMUS(path);
+	if (music == NULL)
+	{
+		LOG("Unable to load music Mix Error: %s\n", Mix_GetError());
+	}
+	else
+	{
+		musics[last_music++] = music;
+	}
+	return music;
+}
+
+//void ModuleAudio::PlayMusic(const char* path) {
+//	uint i = 0;
+//	while (musics[i] != path) {
+//
+//	}
+//}
--- Code/LastResortCode/ModuleAudio.h
+++ Code/LastResortCode/ModuleAudio.h
@@ -1,12 +1,24 @@
 #ifndef __ModuleAudio_H__
 #define __ModuleAudio_H__
 
+#include "Globals.h"
 #include "Module.h"
+#include "SDL_mixer\include\SDL_mixer.h"
 
+#define MAX_MUSICS 3
 #define MAX_SOUNDEFECTS 20
 
-struct Mix_Music;
-struct Mix_Chunk;
+
+enum Music_State {
+	PLAY,
+	STOP, 
+	RESUME
+};
+
+struct Sfx {
+	Mix_Chunk *chunk;
+	char *file;
+};
 
 class ModuleAudio : public Module {
 public:
@@ -16,10 +28,15 @@ public:
 	bool Init();
 	bool CleanUp();
 
+	Mix_Chunk* const LoadSfx(const char* path);
+	Mix_Music* const LoadMUS(const char* path);
+	void PlayMusic(const char* path);
+
 private:
-	Mix_Music *level_music;
+	Mix_Music *musics [MAX_MUSICS];
 	Mix_Chunk *sfx [MAX_SOUNDEFECTS];
 	uint last_chunk = 0;
+	uint last_music = 0;
 };
 
 
