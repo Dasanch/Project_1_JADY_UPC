b0cca42642d262eba5d34bfabae88a4a483e4ad6
Daniel Sanchez <danielsanflo@gmail.com>
2018-04-12 19:41:33 +0200

    Revert "not"
    This reverts commit c856ddd883077052523e10fff16c6b5ef09f3ffa.

--- Code/LastResortCode/ModuleContinue.cpp
+++ Code/LastResortCode/ModuleContinue.cpp
@@ -19,11 +19,6 @@ ModuleContinue::ModuleContinue()
 
 	// BackgroundsRect  
 
-	//Cont
-	BGroundCont.x = 0;
-	BGroundCont.y = 64;
-	BGroundCont.w = 240;
-	BGroundCont.h = 32;
 
 }
 
@@ -37,7 +32,6 @@ bool ModuleContinue::Start()
 	bool ret = true;
 
 	backgroundCont = App->textures->Load("Assets/continue.png");
-	backgroundContBack = App->textures->Load("Assets/continue.png");
 
 	SDL_SetRenderDrawColor(App->render->renderer, 0, 0, 0, 255);
 	SDL_RenderFillRect(App->render->renderer, &BBlack);
@@ -54,7 +48,6 @@ bool ModuleContinue::CleanUp()
 	LOG("Unloading ready scene");
 	App->player->Disable(); // TODO 1: Disable the player module
 	App->textures->Unload(backgroundCont);
-
 	return true;
 }
 
@@ -67,7 +60,6 @@ update_status ModuleContinue::Update()
 
 
 	App->render->Blit(backgroundCont, 110 /*SCREEN_WIDTH/2*/, 100 /*SCREEN_HEIGHT/2*/, &BGroundCont, 0.0f); //MAGIC NUMBERS
-
 
 
 
