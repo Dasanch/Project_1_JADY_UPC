61b6b16046fcbf99d25762cc1d3fdf86f96dbfaa
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-04-16 16:48:46 +0200

    Upgrade to SDL_8 animation.h

--- Code/LastResortCode/Animation.h
+++ Code/LastResortCode/Animation.h
@@ -8,33 +8,71 @@
 class Animation
 {
 public:
+	//Original variables----------------------------------
 	bool loop = true;
+	bool pingpong = false;
 	float speed = 1.0f;
 	SDL_Rect frames[MAX_FRAMES];
 	float current_frame = 0;
 	int last_frame = 0;
 	int loops = 0;
+	enum pingpong
+	{
+		forward,
+		backward
+	} direction = forward;
+
+	//Added variables-------------------------------------
 
 	bool invert = false;
 	bool first_loop = true;
 	bool finished = false;
+
 public:
+	Animation()
+	{}
+
+	Animation(const Animation& anim) : loop(anim.loop), speed(anim.speed), last_frame(anim.last_frame)
+	{
+		SDL_memcpy(&frames, anim.frames, sizeof(frames));
+	}
 
 	void PushBack(const SDL_Rect& rect)
 	{
 		frames[last_frame++] = rect;
 	}
+
 	SDL_Rect&  ReturnFrame(int i)
 	{
 		return frames[i];
 	}
+
 	SDL_Rect& GetCurrentFrame()
 	{
-		current_frame += speed;
-		if(current_frame >= last_frame)
+		switch (direction)
 		{
-			current_frame = (loop) ? 0.0f : last_frame - 1;
-			loops++;
+		case pingpong::forward:
+		{
+			current_frame += speed;
+			if (current_frame >= last_frame)
+			{
+				current_frame = (loop || pingpong) ? 0.0f : last_frame - 1;
+				direction = pingpong ? pingpong::backward : pingpong::forward;
+				loops++;
+			}
+		}
+		break;
+		case pingpong::backward:
+		{
+			current_frame -= speed;
+			if (current_frame <= 0.0f)
+			{
+				current_frame = 0.0f;
+				direction = pingpong::forward;
+				loops++;
+			}
+		}
+		break;
 		}
 
 		return frames[(int)current_frame];
@@ -50,11 +88,14 @@ public:
 		current_frame = 0;
 	}
 
+	//Added methods--------------------------------------------------------------------------------
+
 	SDL_Rect& GetFrame()
 	{
 		return frames[(int)current_frame];
 	}
 
+	//Description: If animation finish switch finished to true (Used in module continue fire animation)
 	SDL_Rect& GetFrameEx()
 	{
 		current_frame += speed;
@@ -65,7 +106,7 @@ public:
 		}
 		return frames[(int)current_frame];
 	}
-
+	//Description: Increment a number of frames to current frame (Implemented in street lights)
 	SDL_Rect& AddFrame(uint added_frames)
 	{
 		uint i = 0;
@@ -78,7 +119,7 @@ public:
 		}
 		return frames[(int)frame];
 	}
-
+	//Description: This runs an animation from 0 to its maximum and from its maximum to 0 infinitely (Implemented in orange laser)
 	SDL_Rect& LoopAnimation()
 	{
 		if (first_loop) {
