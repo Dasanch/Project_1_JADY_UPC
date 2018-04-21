6cd2994c38694b674d559e07ce0391e819651fb8
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-04-21 22:47:14 +0200

    adding middleboss

--- Code/LastResortCode/MiddleBosslvl1.h
+++ Code/LastResortCode/MiddleBosslvl1.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef __ENEMY_METALCRAW_H__
+#define __ENEMY_METALCRAW_H__
+
+#include "Enemy.h"
+
+class Enemy_MetalCraw_Torso : public Enemy
+{
+private:
+	float wave = -1.0f;
+	bool going_up = true;
+	int original_y = 0;
+	Animation dropper;
+
+public:
+
+	Enemy_MetalCraw_Torso(int x, int y);
+
+	void Move();
+};
+
+#endif // __ENEMY_METALCRAW_H__
