4a79ad7329ac6edb8214e5ad152564dc34017148
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-04-22 15:32:09 +0200

    entire middle boss

--- Code/LastResortCode/MiddleBosslvl1.cpp
+++ Code/LastResortCode/MiddleBosslvl1.cpp
@@ -7,16 +7,17 @@
 //-------------------------------------------------------Torso--------------------------------------------------------------------------------------
 Enemy_MetalCraw::Enemy_MetalCraw(int x, int y) : Enemy(x, y)
 {
-	/*torso.PushBack({ 138,372,42,59 });*/
+	torso.PushBack({ 254,373,42,54 });
+	torso.PushBack({ 138,372,42,59 });
 	torso.PushBack({ 180,373,42,57 });
 	torso.speed = 0.0f;
 	animation = &torso;
 
 	Arm.PushBack({ 0,248,46,62 });
-	Arm.PushBack({ 46,248,46,62 });
+	/*Arm.PushBack({ 46,248,46,62 });
 	Arm.PushBack({ 92,248,46,62 });
 	Arm.PushBack({ 138,248,46,62 });
-	Arm.PushBack({ 184,248,46,62 });
+	Arm.PushBack({ 184,248,46,62 });*/
 	Arm.speed = 0.2f;
 	Leg = {222,406,18,24};
 	collider = App->collision->AddCollider({ -97, 6, 42, 42 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
@@ -46,14 +47,16 @@ void  Enemy_MetalCraw::Draw(SDL_Texture* sprites)
 
 	if (animation != nullptr)
 	{
-		App->render->Blit(sprites, position.x, position.y + App->render->relative_camera.y, &(animation->GetCurrentFrame()));
+		
 
-		App->render->Blit(sprites, position.x-42, position.y-5 + App->render->relative_camera.y, &(Arm.GetCurrentFrame()));
-		App->render->FlippedBlit(sprites, position.x+ 42, position.y-5 + App->render->relative_camera.y, &(Arm.GetCurrentFrame()));
+		App->render->Blit(sprites, position.x-43, position.y-6 + App->render->relative_camera.y, &(Arm.GetCurrentFrame()));
+		App->render->FlippedBlit(sprites, position.x+ 39, position.y-6 + App->render->relative_camera.y, &(Arm.GetCurrentFrame()));
 	
 
-		App->render->Blit(sprites, position.x - 5, position.y - 5 + App->render->relative_camera.y, &Leg);
-		App->render->FlippedBlit(sprites, position.x +5, position.y - 5 + App->render->relative_camera.y, &Leg);
+	/*	App->render->Blit(sprites, position.x - 3, position.y + 35 + App->render->relative_camera.y, &Leg);
+		App->render->FlippedBlit(sprites, position.x +27, position.y + 35 + App->render->relative_camera.y, &Leg);*/
+
+		App->render->Blit(sprites, position.x, position.y + App->render->relative_camera.y, &(animation->GetCurrentFrame()));
 	}
 		
 
