3f321bba2c2714bd06a7711eb51ef1b26f4e20c4
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-04-22 20:16:06 +0200

    adding enemies

--- Code/LastResortCode/ModuleStage01.cpp
+++ Code/LastResortCode/ModuleStage01.cpp
@@ -90,8 +90,7 @@ bool ModuleStage01::Start()
 	App->player2->Reset_Positions();
 	//Enemies---------------------------------------------------------------------
 
-<<<<<<< HEAD
-=======
+
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 540, 78);
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 580, 100);
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 610, 78);
@@ -110,24 +109,11 @@ bool ModuleStage01::Start()
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 970, 150);
 
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 980, 120);
-	/*App->enemies->AddEnemy(ENEMY_TYPES::POWERDROPPER, 1976, 136);
-	App->enemies->AddEnemy(ENEMY_TYPES::METALCROW, 1500, SCREEN_HEIGHT,LASER);
-	App->enemies->AddEnemy(ENEMY_TYPES::METALCROW, 4000, SCREEN_HEIGHT);
 
->>>>>>> dcde1e0092084923ed5416d62a0d11586f9dbcf3
-	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 550, 78, LASER);
-	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 570, 102, LASER);
-	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 586, 78, LASER);
-	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 590, 143, LASER);
-	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 1200, 25, SPEED);
-	App->enemies->AddEnemy(ENEMY_TYPES::POWERDROPPER, 1976, 136, LASER);
-<<<<<<< HEAD
-	App->enemies->AddEnemy(ENEMY_TYPES::METALCROW, 300, SCREEN_HEIGHT, LASER);
-
-=======
+	App->enemies->AddEnemy(ENEMY_TYPES::METALCROW, 1500, SCREEN_HEIGHT, LASER);
+	App->enemies->AddEnemy(ENEMY_TYPES::POWERDROPPER, 1976, 136);
 	
-*/
->>>>>>> dcde1e0092084923ed5416d62a0d11586f9dbcf3
+
 
 	//define moveCamera struct values
 	
