90cbd6361e2e4dcf827c5870dfefdf109519cf96
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-04-22 20:43:52 +0200

    enemies

--- Code/LastResortCode/ModuleStage01.cpp
+++ Code/LastResortCode/ModuleStage01.cpp
@@ -106,20 +106,12 @@ bool ModuleStage01::Start()
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 890, 150);
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 930, 150);
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 970, 150);
-<<<<<<< HEAD
+
 
 
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 980, 120);
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 1000, 130);
 	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 1010, 40);
-
-
-
-=======
-	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 970, 120);
-	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 1000, 130);
-	App->enemies->AddEnemy(ENEMY_TYPES::BASIC, 1010, 40);
->>>>>>> origin/master
 
 	App->enemies->AddEnemy(ENEMY_TYPES::METALCROW, 1500, SCREEN_HEIGHT, LASER);
 	App->enemies->AddEnemy(ENEMY_TYPES::POWERDROPPER, 1976, 136);
